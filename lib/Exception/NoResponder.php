94f4c2f2ff69f0b8ab3d15ce3b3423af46880388
Olivier Laviale <olivier.laviale@gmail.com>
2021-05-30 17:16:14 +0200

    Introduce the notion of Responder

---
+++ lib/Exception/NoResponder.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the ICanBoogie package.
+ *
+ * (c) Olivier Laviale <olivier.laviale@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ICanBoogie\HTTP\Exception;
+
+use ICanBoogie\HTTP\Exception;
+use LogicException;
+
+/**
+ * Thrown when there's no responder available for a request.
+ */
+class NoResponder extends LogicException implements Exception
+{
+
+}
---
+++ lib/Responder.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the ICanBoogie package.
+ *
+ * (c) Olivier Laviale <olivier.laviale@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ICanBoogie\HTTP;
+
+use Throwable;
+
+/**
+ * Responds to a request.
+ */
+interface Responder
+{
+	/**
+	 * @throws Throwable if the response cannot be produced.
+	 */
+	public function respond(Request $request): Response;
+}
